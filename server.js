5074f62ad39db033af31ca7601c5910067df8d76
Shuvo-Khan <2021331069@student.sust.edu>
2025-12-05 12:52:18 +0600

    Complete controllers & models & routes setup

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+// server/server.js
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Initialize Express App
-const app = express()
+const app = express();
+
+// database connect (top-level await)
+await connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> res.send("Server is running"))
+// Routes
+app.get('/', (req, res) => res.send("Server is running"));
+app.use('/api/user',userRouter)
 
+// Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
